0a8610924c49bcd29c29006f8ead7eb46e448014
Jorger Sillero <jorgesilleromanchon@gmail.com>
2022-12-01 17:23:13 +0100

    feat: Visualizar datos de votación tipo si/no
    Muestra una gráfica según el tipo de votación. No funciona.

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -12,7 +12,7 @@ class VisualizerView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         vid = kwargs.get('voting_id', 0)
-        tipo = "normla"
+        tipo = "normal"
         context['tipo'] = tipo
 
         try:
